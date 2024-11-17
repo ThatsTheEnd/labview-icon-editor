a2f76504bf0b6736f222f9d4f82715df6be7dc6f
sergiov-ni <sergio.velderrain@ni.com>
2024-11-16 19:07:11 -0800

    Update buildniiconeditor64LVAddonVIPackage.bat

--- Tooling/deployment/buildniiconeditor64LVAddonVIPackage.bat
+++ Tooling/deployment/buildniiconeditor64LVAddonVIPackage.bat
@@ -8,32 +8,30 @@ set "IconEditorBuildSpec="Editor Packed Library""
 set "LabVIEWPath="C:\Program Files\National Instruments\LabVIEW 2021\LabVIEW.exe""
 
 REM Delete built LVAddon
-C:\
-dir
-cd /d %AGENT_HOMEDIRECTORY%
+cd /d C:\Program Files (x86)\National Instruments\Shared\LabVIEW CLI
 dir
 
 REM Create INI token localhost.LibraryPath on LabVIEW.ini that points to the development folder
-start LabVIEWCLI -OperationName RunVI -VIPath "%RelativePath%Tooling\deployment\NIPackage\CreateLVINILocalHostKey.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog1.txt" -Verbosity Diagnostic -LogToConsole TRUE
+LabVIEWCLI -OperationName RunVI -VIPath "%RelativePath%Tooling\deployment\NIPackage\CreateLVINILocalHostKey.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog1.txt" -Verbosity Diagnostic -LogToConsole TRUE
 
-start LabVIEWCLI -OperationName CloseLabVIEW -PortNumber %VIServerPort%
+LabVIEWCLI -OperationName CloseLabVIEW -PortNumber %VIServerPort%
 
 REM Prepare labview to use icon editor to use source code
-start LabVIEWCLI -OperationName RunVI -VIPath "%RelativePath%\Tooling\Prepare LV to Use Icon Editor Source v2.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog2.txt" -Verbosity Diagnostic -LogToConsole TRUE
+LabVIEWCLI -OperationName RunVI -VIPath "%RelativePath%\Tooling\Prepare LV to Use Icon Editor Source v2.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog2.txt" -Verbosity Diagnostic -LogToConsole TRUE
 
 REM Modify the icon editor build spec to deploy to LVAddons
-start LabVIEWCLI -OperationName RunVI -VIPath %ModifyIconEditorProjectVIPath% "%RelativePath%lv_icon_editor.lvproj" %IconEditorBuildSpec% -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog3.txt" -Verbosity Diagnostic -LogToConsole TRUE
+LabVIEWCLI -OperationName RunVI -VIPath %ModifyIconEditorProjectVIPath% "%RelativePath%lv_icon_editor.lvproj" %IconEditorBuildSpec% -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog3.txt" -Verbosity Diagnostic -LogToConsole TRUE
 
-start LabVIEWCLI -OperationName CloseLabVIEW -PortNumber %VIServerPort%
+LabVIEWCLI -OperationName CloseLabVIEW -PortNumber %VIServerPort%
 
 REM Build the PPL
-start LabVIEWCLI -OperationName ExecuteBuildSpec -ProjectPath "%RelativePath%lv_icon_editor.lvproj" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog4.txt" -Verbosity Detailed -LogToConsole TRUE -TargetName "My Computer" -BuildSpecName %IconEditorBuildSpec%
+LabVIEWCLI -OperationName ExecuteBuildSpec -ProjectPath "%RelativePath%lv_icon_editor.lvproj" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog4.txt" -Verbosity Detailed -LogToConsole TRUE -TargetName "My Computer" -BuildSpecName %IconEditorBuildSpec%
 
 REM Create JSON file on LVAddon
 
-start LabVIEWCLI -OperationName RunVI -VIPath "%RelativePath%Tooling\deployment\NIPackage\CreateLVAddonJSONfile.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog5.txt" -Verbosity Diagnostic -LogToConsole TRUE
+LabVIEWCLI -OperationName RunVI -VIPath "%RelativePath%Tooling\deployment\NIPackage\CreateLVAddonJSONfile.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog5.txt" -Verbosity Diagnostic -LogToConsole TRUE
 
-start LabVIEWCLI -OperationName CloseLabVIEW -PortNumber %VIServerPort%
+LabVIEWCLI -OperationName CloseLabVIEW -PortNumber %VIServerPort%
 
 echo "All LabVIEW CLI commands completed successfully."
 echo "Script finished"
