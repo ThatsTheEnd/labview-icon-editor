d753cf9eea65f4c3af430ac9d42142a87ce3e1d0
sergiov-ni <sergio.velderrain@ni.com>
2024-11-16 16:46:43 -0800

    g
    gt

--- Tooling/deployment/buildniiconeditor64LVAddonVIPackage.bat
+++ Tooling/deployment/buildniiconeditor64LVAddonVIPackage.bat
@@ -6,32 +6,32 @@ set "ModifyIconEditorProjectVI=Tooling\deployment\NIPackage\ModifyProjectDeployL
 set "ModifyIconEditorProjectVIPath="%RelativePath%%ModifyIconEditorProjectVI%""
 set "IconEditorBuildSpec="Editor Packed Library""
 set "LabVIEWPath="C:\Program Files\National Instruments\LabVIEW 2021\LabVIEW.exe""
-set "LabVIEWCLI="C:\Program Files (x86)\National Instruments\Shared\LabVIEW CLI\LabVIEWCLI.exe""
 
+REM Delete built LVAddon
+cd /d C:\Program Files\NI\LVAddons
+rmdir /s /q niiconeditor64
 
 REM Create INI token localhost.LibraryPath on LabVIEW.ini that points to the development folder
+LabVIEWCLI -OperationName RunVI -VIPath "%RelativePath%Tooling\deployment\NIPackage\CreateLVINILocalHostKey.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog1.txt" -Verbosity Diagnostic -LogToConsole TRUE
 
-
-%LabVIEWCLI% -OperationName RunVI -VIPath "%RelativePath%Tooling\deployment\NIPackage\CreateLVINILocalHostKey.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog1.txt" -Verbosity Diagnostic -LogToConsole TRUE
-
-%LabVIEWCLI% -OperationName CloseLabVIEW -PortNumber %VIServerPort%
+LabVIEWCLI -OperationName CloseLabVIEW -PortNumber %VIServerPort%
 
 REM Prepare labview to use icon editor to use source code
-%LabVIEWCLI% -OperationName RunVI -VIPath "%RelativePath%\Tooling\Prepare LV to Use Icon Editor Source v2.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog2.txt" -Verbosity Diagnostic -LogToConsole TRUE
+LabVIEWCLI -OperationName RunVI -VIPath "%RelativePath%\Tooling\Prepare LV to Use Icon Editor Source v2.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog2.txt" -Verbosity Diagnostic -LogToConsole TRUE
 
 REM Modify the icon editor build spec to deploy to LVAddons
-%LabVIEWCLI% -OperationName RunVI -VIPath %ModifyIconEditorProjectVIPath% "%RelativePath%lv_icon_editor.lvproj" %IconEditorBuildSpec% -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog3.txt" -Verbosity Diagnostic -LogToConsole TRUE
+LabVIEWCLI -OperationName RunVI -VIPath %ModifyIconEditorProjectVIPath% "%RelativePath%lv_icon_editor.lvproj" %IconEditorBuildSpec% -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog3.txt" -Verbosity Diagnostic -LogToConsole TRUE
 
 LabVIEWCLI -OperationName CloseLabVIEW -PortNumber %VIServerPort%
 
 REM Build the PPL
-%LabVIEWCLI% -OperationName ExecuteBuildSpec -ProjectPath "%RelativePath%lv_icon_editor.lvproj" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog4.txt" -Verbosity Detailed -LogToConsole TRUE -TargetName "My Computer" -BuildSpecName %IconEditorBuildSpec%
+LabVIEWCLI -OperationName ExecuteBuildSpec -ProjectPath "%RelativePath%lv_icon_editor.lvproj" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog4.txt" -Verbosity Detailed -LogToConsole TRUE -TargetName "My Computer" -BuildSpecName %IconEditorBuildSpec%
 
 REM Create JSON file on LVAddon
 
-%LabVIEWCLI% -OperationName RunVI -VIPath "%RelativePath%Tooling\deployment\NIPackage\CreateLVAddonJSONfile.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog5.txt" -Verbosity Diagnostic -LogToConsole TRUE
+LabVIEWCLI -OperationName RunVI -VIPath "%RelativePath%Tooling\deployment\NIPackage\CreateLVAddonJSONfile.vi" -LabVIEWPath %LabVIEWPath% -PortNumber %VIServerPort% -LogFilePath "C:\Users\Public\CLIlog5.txt" -Verbosity Diagnostic -LogToConsole TRUE
 
-%LabVIEWCLI% -OperationName CloseLabVIEW -PortNumber %VIServerPort%
+LabVIEWCLI -OperationName CloseLabVIEW -PortNumber %VIServerPort%
 
 echo "All LabVIEW CLI commands completed successfully."
 echo "Script finished"
